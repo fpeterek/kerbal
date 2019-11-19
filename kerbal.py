5de5199ec60343b4266d11b52b98c3bf4ca2732b
Filip Peterek <fpeterek@seznam.cz>
2019-11-19 16:02:39 +0100

    Kerbal class

--- kerbal.py
+++ kerbal.py
@@ -3,45 +3,49 @@ from wind import Wind
 from window import Window
 import time
 
-def millis():
-    return time.time_ns() // 1_000_000
 
-class Spot:
+class Kerbal:
 
-    def __init__(self, x, y, rad):
-        self.x = x
-        self.y = y
-        self.rad = rad
+    @staticmethod
+    def millis():
+        return time.time_ns() // 1_000_000
 
-    def draw(self, canvas):
-        return canvas.create_oval(self.x - self.rad, self.y - self.rad,
-                                  self.x + self.rad, self.y + self.rad,
-                                  fill='#000000')
+    def __init__(self):
+        self.win = Window((1600, 900))
+        self.wind = Wind()
+        self.rocket = Rocket(x=120, y=300, width=50, height=100)
+        self.wind_v = self.wind.tick(0)
+        self.last_time = Kerbal.millis()
+        self.win.add_handler('a', lambda x: self.left())
+        self.win.add_handler('d', lambda x: self.right())
+
+    def left(self):
+        self.rocket.tilt(1, 'left')
+
+    def right(self):
+        self.rocket.tilt(1, 'right')
+
+    def up(self):
+        pass
+
+    def run(self):
+        while self.win.open:
+            c_time = Kerbal.millis()
+            delta = (self.last_time - c_time) / 1000
+            self.tick(delta)
+            self.last_time = c_time
+
+    def tick(self, timedelta: float):
+        self.wind_v = self.wind.tick(timedelta)
+        self.draw()
+
+    def draw(self):
+        self.win.clear()
+        self.win.draw(self.rocket)
+        self.win.update()
 
 
 if __name__ == '__main__':
-    win = Window((1600, 900))
-    wind = Wind()
-    win.add_handler('d', lambda r: rocket.tilt(1, 'right'))
-    win.add_handler('a', lambda r: rocket.tilt(1, 'left'))
-    wind_v = wind.tick(0)
-
-    spots = [Spot(100, 100, 40), Spot(200, 300, 80), Spot(600, 600, 120)]
-    rocket = Rocket(x=120, y=300, width=50, height=100)
-
-    last_time = millis()
-
-    while win.open:
-        c_time = millis()
-        delta = (c_time - last_time) / 1000
-        n_wind = wind.tick(delta)
-        if n_wind != wind_v:
-            print(f'new wind: {wind_v}')
-
-        wind_v = n_wind
-        win.draw(rocket)
-        win.update()
-        time.sleep(0.02)
-        win.clear()
-        last_time = c_time
+    kerbal = Kerbal()
+    kerbal.run()
 
