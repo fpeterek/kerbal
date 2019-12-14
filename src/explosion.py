e8984745dd6bff6f9a3f8e9e5d2649d427f42ed7
Filip Peterek <fpeterek@seznam.cz>
2019-12-14 19:09:47 +0100

    Load explosion spritesheet only once

--- src/explosion.py
+++ src/explosion.py
@@ -9,18 +9,26 @@ class Explosion:
     sprites = 12
     period = 0.1
 
+    sheet = None
+
     @property
     def keepalive(self) -> bool:
         return self.phase < Explosion.sprites
 
-    def __init__(self, x, y):
+    @staticmethod
+    def load_spritesheet():
         img: Image.Image = Image.open('resources/explosion.png')
-        img = img.resize((Explosion.width*Explosion.sprites, Explosion.height), Image.NONE)
+        img = img.resize((Explosion.width * Explosion.sprites, Explosion.height), Image.NONE)
         count = Explosion.sprites
-        self.sprites = [
-            img.crop((i*Explosion.width, 0, Explosion.width*(i+1), Explosion.height)) for i in range(0, count)
+        sheet = [
+            img.crop((i * Explosion.width, 0, Explosion.width * (i + 1), Explosion.height)) for i in range(0, count)
         ]
-        self.sprites = list(map(lambda image: ImageTk.PhotoImage(image), self.sprites))
+        Explosion.sheet = list(map(lambda image: ImageTk.PhotoImage(image), sheet))
+
+    def __init__(self, x, y):
+        if not Explosion.sheet:
+            Explosion.load_spritesheet()
+
         self.phase = 0
         self.counter = 0.0
         self.x = x - Explosion.width // 2
@@ -33,4 +41,4 @@ class Explosion:
 
     def draw(self, canvas: tkinter.Canvas):
         if self.keepalive:
-            canvas.create_image(self.x, self.y, image=self.sprites[self.phase], anchor=tkinter.NW)
+            canvas.create_image(self.x, self.y, image=Explosion.sheet[self.phase], anchor=tkinter.NW)
